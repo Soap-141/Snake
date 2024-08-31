174c9061b78cf98c2bbf55d809b711ae5d6a2957
Thomas Labrecque <thomas.labrecque@nventive.com>
2024-08-31 12:54:48 -0400

    feat: Basic Snake

---
+++ Snake/SnakePart.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Snake;
+
+/// <summary>
+/// The directions the snake can move.
+/// </summary>
+public enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+/// <summary>
+/// A part of the snake.
+/// </summary>
+public struct SnakePart
+{
+    public Point Position { get; }
+
+    public Direction? Direction { get; set; }
+
+    public SnakePart(Point position, Direction? direction)
+    {
+        Position = position;
+        Direction = direction;
+    }
+}
